c1ed54a842fd596c84667ff2062c466b31030669
nomennescio <nomennescio@users.noreply.github.com>
2019-07-29 11:15:38 +0200

    Expand tests with verbose and quiet flags and passing of files on commandline

--- test/run-tests.py
+++ test/run-tests.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 # Copyright 2019 nomennescio
-# MIT license
 
-import glob, os, re
+import glob, os, re, sys
 
-passed, failed = 0, 0
+passed, failed, verbose, quiet = 0, 0, 0, 0
 
 def run (forthfile):
     return os.popen ("gforth ../ttester-codewars.4th " + forthfile + " -e bye").readlines ()
@@ -12,25 +11,46 @@ def run (forthfile):
 def timeless (lines):
     return [re.sub(r'\d+', '0', l) if "<COMPLETEDIN::>" in l else l for l in lines]
 
-def test (base):
+def test (filename):
     global passed, failed
-    if (timeless (run (base + ".4th")) == timeless (open (base + ".expected").readlines ())):
-        print ("Pass: " + base)
+    actual = run (filename)
+    expected = open (os.path.splitext (filename)[0] + ".expected").readlines ()
+    if (timeless (actual) == timeless (expected)):
+        if verbose:
+            print ("Pass: " + filename)
         passed += 1
     else:
-        print ("Fail: " + base)
+        if (not quiet):
+            print ("Fail: {0}\n==8<--".format (filename, ""))
+        sys.stdout.write (''.join (actual))
+        if (not quiet):
+            print ("\n==8<--")
         failed += 1
 
+# change to script directory
 prevdir = os.getcwd ()
 os.chdir (os.path.dirname (os.path.abspath (__file__)))
 
-files = [os.path.splitext (f) for f in glob.glob ("*.4th")]
-for f,_ in files:
+# parse flags
+args=sys.argv[1:]
+if (len (args) and args[0]=="-v"):
+    verbose=1
+    args.pop (0)
+if (len (args) and args[0]=="-q"):
+    quiet=1
+    args.pop (0)
+
+# test named files, or all Forth files in directory
+files = args if len (args) else glob.glob ("*.4th")
+for f in files:
     test (f)
 
-if (failed==0):
-    print ("All {0} tests were successful".format (passed))
-else:
-    print ("{0} out of {1} tests failed".format (failed, failed+passed))
-    
+# show totals
+if (verbose):
+    if (failed==0):
+        print ("All {0} tests were successful".format (passed))
+    else:
+        print ("{0} out of {1} tests failed".format (failed, failed+passed))
+
+# restore directory
 os.chdir (prevdir)
