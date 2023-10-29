ba28d69cad18ab6ca1843c35b1f8e161b1f99274
nomennescio <nomennescio@users.noreply.github.com>
2023-10-30 00:07:54 +0100

    New load order

--- example/run-tests.sh
+++ example/run-tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+# Copyright 2018-2023 nomennescio
 
 example_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)
 
 cd "$example_dir"
-gforth preloaded.4th solution.4th ../testest.4th tests.4th -e bye
+gforth ../preamble.4th ../testest.4th ../protect.4th preloaded.4th solution.4th ../prepare.4th tests.4th -e bye
